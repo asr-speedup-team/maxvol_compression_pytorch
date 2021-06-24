8e1414c368226613a015d81c9cdbf347b47a0235
darayavaus <darayavaus.chern@yandex.ru>
2021-06-24 19:21:25 +0300

    Updates: 	- Dummy model uses conv1d insted of conv2d 	- TrainingDummy notebook updated 	- VBMF notebook updated 	- ExampleMaxvol notebook has conv1d layer compression with toeplitz matrix

--- utils/dummy.py
+++ utils/dummy.py
@@ -31,24 +31,28 @@ class DummyDatasetCifar10:
                                                       drop_last=True,
                                                       num_workers=2)
         self.classes = np.array(['plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck'])
+                                 'deer', 'dog', 'frog', 'horse',
+                                 'ship', 'truck'])
 
 
 class DummyModelCifar10(nn.Module):
     def __init__(self):
         super().__init__()
         self.num_classes = 10
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, self.num_classes)
+        self.conv1 = nn.Conv1d(3, 15, 100)
+        self.pool = nn.MaxPool1d(4, 4)
+        # (, 15, 232)
+        self.conv2 = nn.Conv1d(15, 30, 50)
+        # (, 30, 45)
+        self.fc1 = nn.Linear(30 * 45, 500)
+        self.fc2 = nn.Linear(500, 250)
+        self.fc3 = nn.Linear(250, self.num_classes)
 
     def forward(self, x):
+        x = torch.flatten(x, -2)  # flatten last 2 dimensions
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
